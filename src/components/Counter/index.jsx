bfd279384b74b59f5703d748cdd8194d68e3f1c6
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-01 22:00:13 +0300

    add store

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Counter = (props) => {
+  return (
+    <div>
+      <p>Count : {}</p>
+      <p>Step : {}</p>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
