ad998dd1b031e047adc0fdb00b15409e5bf1782d
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-16 20:57:50 +0300

    add get users

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+async function getUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  return users;
+}
+
+export default getUsers;
