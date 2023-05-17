8c152ab0584728fc61be64d2810db84474de61ca
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-05-17 21:45:16 +0300

    axios example

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+
+// export async function getUsers() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const users = await response.json();
+//   return users;
+// }
+
 export async function getUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const { data: users } = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
   return users;
 }
